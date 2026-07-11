a7ea2d5aff42aef6fa596540c3135cdaf72b59e1
Md Sameer Ansari <md.sameer.np@gmail.com>
2026-07-11 22:51:38 +0530

    fix proxy.ts: rename middleware() to proxy()

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr"
 import { NextResponse, type NextRequest } from "next/server"
 
-export async function updateSession(request: NextRequest) {
+async function updateSession(request: NextRequest) {
   let response = NextResponse.next({
     request: { headers: request.headers },
   })
@@ -28,7 +28,7 @@ export async function updateSession(request: NextRequest) {
   return { response, user }
 }
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { response, user } = await updateSession(request)
   const { pathname } = request.nextUrl
 
